8668c58c84fcb8999373c85952f9c15c7ff86ca1
julia98percent <ji_0315@naver.com>
2021-11-17 23:30:19 +0900

    디자인 패턴 적용

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Switch, BrowserRouter, Route, Link, Redirect } from "react-router-dom";
+import MainPage from "./routes/MainPage";
+import PrevPage from "./routes/PrevPage";
 function App() {
+  const [q, setQ] = useState([]);
+  const [a, setA] = useState([]);
+
+  useEffect(() => {
+    const call = async () => {
+      const key = "e75f4f4f23d735668faf09f0ffff8123";
+      const response = await axios.get(
+        `https://www.career.go.kr/inspct/openapi/test/questions?apikey=${key}&q=6`
+      );
+      setQ(response.data.RESULT);
+    };
+    call();
+  }, [q]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/test" component={MainPage} />
+          {/* <Route path="/result" component={ResultPage} /> */}
+          <Route exact path="/" component={PrevPage} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/routes/MainPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+function MainPage() {
+  return (
+    <div className="main">
+      <h2>직업가치관검사</h2>
+      <input />
+      <button>검사중</button>
+    </div>
+  );
+}
+
+export default MainPage;
